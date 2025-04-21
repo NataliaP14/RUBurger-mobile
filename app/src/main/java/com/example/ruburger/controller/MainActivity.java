ab8983a00982c00a9094ffbbf2d8e1a8479ed237
Natalia Peguero <pegueronatalia@gmail.com>
2025-04-20 23:10:36 -0400

    frontend ting

--- app/src/main/java/com/example/ruburger/controller/MainActivity.java
+++ app/src/main/java/com/example/ruburger/controller/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ruburger.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        LinearLayout burgerBox = findViewById(R.id.burgerBox);
+        LinearLayout sandwichBox = findViewById(R.id.sandwichBox);
+        LinearLayout beverageBox = findViewById(R.id.beverageBox);
+        LinearLayout sideBox = findViewById(R.id.sideBox);
+
+
+        burgerBox.setOnClickListener(v -> startActivity(new Intent(this, BurgerController.class)));
+        sandwichBox.setOnClickListener(v -> startActivity(new Intent(this, SandwichController.class)));
+        beverageBox.setOnClickListener(v -> startActivity(new Intent(this, BeverageController.class)));
+        sideBox.setOnClickListener(v -> startActivity(new Intent(this, SideController.class)));
     }
 }
